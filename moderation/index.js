b8be45d24d7220ef1fc4082df33a9008a1cea62c
rashidsidhik <rashidps44@gmail.com>
2023-08-31 17:57:02 +0530

    first commit

---
+++ moderation/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.post('/events', async(req,res) => {
+     const {type ,data} = req.body;
+
+     if(type === 'CommentCreated'){
+        console.log("called");
+        const status = data.content.includes("apple")?'rejected': 'approved';
+        console.log(status);
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data:{
+                id:data.id,
+                postId:data.postId,
+                status,
+                content:data.content
+            }
+        })
+     }
+
+     res.send({})
+})
+
+app.listen(4003, () => {
+    console.log("Listening on port 4003")
+})
